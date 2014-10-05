bd575cb430f0757d4f9f4d1b75a8cd1c58baf7ff
Sciatic126 <oarnaout@gmail.com>
2014-10-04 19:22:48 -0500

    Started qanda app and first failed UT

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,31 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Omar' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_show_a_question(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('Welcome to qbank', self.browser.title)
+		self.fail('Finish the test')
+
+# Student arrives at qbank website
+
+
+# A wild question appears! with four potential answers
+
+# A first attempt at an answer is made, wrong answer!
+
+# A second attmept, correct answer. Text changes to green and an explination appears below
+
+# Click on next button, which has been enabled
+
+# Student moves on to the next question
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
