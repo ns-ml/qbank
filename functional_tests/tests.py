f606da3b9d642ffdd34e2bc4b217e859f30987a6
Sciatic126 <oarnaout@gmail.com>
2014-10-04 21:02:34 -0500

    Added a button on the homepage that redirects to the only question

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+from django.test import LiveServerTestCase
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -9,15 +10,22 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def test_can_show_a_question(self):
-		self.browser.get('http://localhost:8000')
-		self.assertIn('Welcome to Q Bank', self.browser.title)
-		self.fail('Finish the test')
-
 # Student arrives at qbank website
 
+	def test_can_show_a_question(self):
+		self.browser.get(self.live_server_url)
+		self.assertIn('Welcome to Q Bank', self.browser.title)
 
+# Student clicks the start button and is taken to the first question
+
+		self.browser.find_element_by_id("submit").click()
+		current_url = self.browser.current_url
+		self.assertEqual(current_url, 'http://localhost:8081/the_only_question/')
+
+		
 # A wild question appears! with four potential answers
+
+
 
 # A first attempt at an answer is made, wrong answer!
 
@@ -26,6 +34,4 @@ class NewVisitorTest(unittest.TestCase):
 # Click on next button, which has been enabled
 
 # Student moves on to the next question
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+		self.fail('Finish the test')
