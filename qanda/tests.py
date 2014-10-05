ef378692e8d632c061e78cff8f9c904e7c186f1d
Sciatic126 <oarnaout@gmail.com>
2014-10-05 05:27:29 -0500

    Added Q&A view test UT, expected fail

--- qanda/tests.py
+++ qanda/tests.py
@@ -16,3 +16,23 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
+
+class QuestionandAnswerViewTest(TestCase):
+	
+	def test_save_and_retrieve_an_answer(self):
+		answer = Answer()
+		answer.text = "First answer ever"
+		answer.save()
+
+		second_answer = Answer()
+		second_answer.text = "Second answer!"
+		second_answer.save()
+
+		saved_answers = Answer.objects.all()
+		self.assertEqual(saved_answers.count(), 2)
+
+		first_saved_answer = saved_answers[0]
+		second_saved_answer = saved_answers[1]
+		self.assertEqual('First answer ever', first_saved_answer.text)
+		self.assertIn('Second', second_saved_answer.text)
+		
