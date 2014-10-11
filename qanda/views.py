0dd0684fb71ca7b14b54b8f55a20e2e1c4ff7345
Sciatic126 <oarnaout@gmail.com>
2014-10-11 06:43:01 -0500

    Passing UT and FT with get object or 404 for explination

--- qanda/views.py
+++ qanda/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from qanda.models import Answer, Question, Explination
 
 # Create your views here.
@@ -15,7 +15,7 @@ def view_question(request, question_id):
 def view_answer(request, question_id):
 	question_stem = Question.objects.get(id=question_id)
 	next_question_id = int(question_id)+1
-	explination_text = Explination.objects.get(question=question_stem)
+	explination_text = get_object_or_404(Explination, question=question_stem)
 	correct_answer = Answer.objects.filter(question=question_stem, correct=True)
 	return render (request, 'view_answer.html', {
 		'correct_answer': correct_answer,
