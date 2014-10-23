8952751e1ac9697e2f95d10a02aec89137e06b5b
Sciatic126 <oarnaout@gmail.com>
2014-10-23 10:00:30 -0500

    Refactored hardcoded URLs in the views

--- qbank/urls.py
+++ qbank/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'qanda.views.home_page', name='home'),
-    url(r'^questions/(\d+)/$', 'qanda.views.check_answer', name='view_question'),
-    url(r'^questions/(\d+)/answer$', 'qanda.views.view_answer', name='view_answer'),
+    url(r'^questions/', include('qanda.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
